5210f1dd15765900d9e6fb15ea6ba9af5e8428d4
Gaurav Ashish <71788323+rghvgrv@users.noreply.github.com>
2024-09-02 01:22:52 +0530

    Add files via upload

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Adds the DB context to the DI container and enables displaying the DB related Exceptions
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+app.MapGet("/todoitems",async(TodoDb db) => await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/complete", async(TodoDb db) => await db.Todos.Where(t => t.IsComplete == true).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+var message = builder.Configuration.GetValue<string>("message");
+app.MapGet("/message", () => message);
+
+
+app.Run();
